0521bafde203855ea7f67e0665075c89adb3a557
yessinekarmous <133720580+yessinekarmous@users.noreply.github.com>
2024-01-23 11:58:33 +0100

    All the Models

---
+++ choreTracker/Models/Job.cs
@@ -0,0 +1,29 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace choreTracker.Models;
+
+public class Job{
+
+    [Key]
+    public int JobId {get;set;}
+    
+    [MinLength(3, ErrorMessage = "The minimum length for the Title field is 3 characters")]
+    public string Title{get;set;}
+
+    [MinLength(10, ErrorMessage = "The minimum length for the Description field is 10 characters")]
+    public string  Description{get;set;}
+    
+    [MinLength(1, ErrorMessage = "Location must not be blank")]
+    public string Location{get;set;}
+
+    public DateTime CreatedAt{get;set;}=DateTime.Now;
+    public DateTime UpdatedAt{get;set;}=DateTime.Now;
+
+    public int UserId {get;set;}
+    public User? Creator {get;set;}
+
+    public List<Favorite> myUsers{get;set;}=new List<Favorite>();
+
+
+}
--- choreTracker/Models/MyContext.cs
+++ choreTracker/Models/MyContext.cs
@@ -5,4 +5,7 @@ namespace choreTracker.Models;
 public class MyContext : DbContext 
 { 
     public MyContext(DbContextOptions options) : base(options) { }
+    public DbSet<User> Users { get; set; } 
+    public DbSet<Job> Jobs { get; set; } 
+    public DbSet<Favorite> Favorites { get; set; } 
 }
