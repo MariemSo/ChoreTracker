899e040b3bb1857bf377e35bd94794267eae4ab3
yessinekarmous <133720580+yessinekarmous@users.noreply.github.com>
2024-01-23 11:57:55 +0100

    Add files via upload
    The Controllers

--- choreTracker/Controllers/HomeController.cs
+++ choreTracker/Controllers/HomeController.cs
@@ -17,9 +17,93 @@ public class HomeController : Controller
     {
         return View();
     }
-
-    public IActionResult Privacy()
+    [HttpPost]
+    public IActionResult Register(User newUser)
     {
+        if (ModelState.IsValid)
+        {
+            if (_context.Users.Any(u => u.Email == newUser.Email))
+            {
+                ModelState.AddModelError("Email", "Email already used .");
+                return View("Index");
+            }
+            PasswordHasher<User> hasher = new PasswordHasher<User>();
+            newUser.Password = hasher.HashPassword(newUser, newUser.Password);
+            _context.Add(newUser);
+            _context.SaveChanges();
+            HttpContext.Session.SetInt32("userId", newUser.UserId);
+            HttpContext.Session.SetString("name", newUser.FName);
+            return RedirectToAction("Dashboard");
+        }
+        return View("Index");
+    }
+    [HttpPost]
+    public IActionResult Login(LogUser newLogUser)
+    {
+        if (ModelState.IsValid)
+        {
+            User userfromdb = _context.Users.FirstOrDefault(u => u.Email == newLogUser.LogEmail);
+            if (userfromdb == null)
+            {
+                ModelState.AddModelError("LogEmail", "Invalid Login Attempt");
+                return View("Index");
+            }
+            PasswordHasher<LogUser> hasher = new PasswordHasher<LogUser>();
+            var result = hasher.VerifyHashedPassword(newLogUser, userfromdb.Password, newLogUser.LogPassword);
+            if (result == 0)
+            {
+                ModelState.AddModelError("LogEmail", "Invalid Login Attempt");
+                return View("Index");
+            }
+            HttpContext.Session.SetInt32("userId", userfromdb.UserId);
+            HttpContext.Session.SetString("name", userfromdb.FName);
+            return RedirectToAction("Dashboard");
+        }
+        return View("Index");
+    }
+
+    [HttpGet("logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet("dashboard")]
+    public IActionResult Dashboard()
+    {
+        if ((HttpContext.Session.GetInt32("userId") == null))
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    [HttpGet("addJob")]
+    public IActionResult AddJob()
+    {
+        if ((HttpContext.Session.GetInt32("userId") == null))
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+    [HttpGet("view/{id}")]
+    public IActionResult ViewJob(int id)
+    {
+        if ((HttpContext.Session.GetInt32("userId") == null))
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+    [HttpGet("edit/{id}")]
+    public IActionResult EditJob(int id)
+    {
+        if ((HttpContext.Session.GetInt32("userId") == null))
+        {
+            return RedirectToAction("Index");
+        }
         return View();
     }
 
