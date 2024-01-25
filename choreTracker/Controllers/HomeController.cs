ade4953db09af328024718cbe93f5f7a5ee8ed7c
MairemSo <souayah.mariem@gmail.com>
2024-01-25 20:39:00 +0100

    View AddJob/Edit /Login/Dashboard (two last not complete)

--- choreTracker/Controllers/HomeController.cs
+++ choreTracker/Controllers/HomeController.cs
@@ -1,15 +1,23 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+
+
 using choreTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
 
 namespace choreTracker.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly MyContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
+    public HomeController(ILogger<HomeController> logger, MyContext context )
+    {   
+        _context = context;
         _logger = logger;
     }
 
@@ -72,41 +80,85 @@ public class HomeController : Controller
     [HttpGet("dashboard")]
     public IActionResult Dashboard()
     {
-        if ((HttpContext.Session.GetInt32("userId") == null))
+        if (HttpContext.Session.GetInt32("userId") == null)
         {
             return RedirectToAction("Index");
         }
-        return View();
+        List<Job> AllJobs = _context.Jobs.Include(c=>c.Creator).ToList();
+
+        return View(AllJobs);
     }
 
-    [HttpGet("addJob")]
+    [HttpGet("addjob")]
     public IActionResult AddJob()
     {
-        if ((HttpContext.Session.GetInt32("userId") == null))
+        if (HttpContext.Session.GetInt32("userId") == null)
         {
-            return RedirectToAction("Index");
+            return RedirectToAction("Dashboard");
         }
         return View();
     }
-    [HttpGet("view/{id}")]
-    public IActionResult ViewJob(int id)
+    [HttpPost("jobs/create")]
+    public IActionResult CreateJob(Job newJob)
     {
-        if ((HttpContext.Session.GetInt32("userId") == null))
+        if (ModelState.IsValid)
         {
-            return RedirectToAction("Index");
+                _context.Add(newJob);
+                _context.SaveChanges();
+            return RedirectToAction("Dashboard");
         }
-        return View();
+        return View("AddJob", newJob);
     }
-    [HttpGet("edit/{id}")]
-    public IActionResult EditJob(int id)
+    [HttpGet("viewJob/{JobId}")]
+    public IActionResult ViewJob(int JobId)
     {
-        if ((HttpContext.Session.GetInt32("userId") == null))
+        if (HttpContext.Session.GetInt32("userId") == null)
         {
             return RedirectToAction("Index");
         }
         return View();
     }
+    [HttpGet("edit/{JobId}")]
+    public IActionResult EditJob(int JobId)
+    {
+        if (HttpContext.Session.GetInt32("userId") == null)
+        {
+        return RedirectToAction("Index");
+        }
+            Job? jobToEdit = _context.Jobs
+            .SingleOrDefault(d => d.JobId == JobId);
+        return View( jobToEdit);
+    }
+     [HttpPost("/jobs/update/{JobId}")]
+    public IActionResult UpdateJob(int JobId, Job newestJob)
+    {
+        Job? oldJob = _context.Jobs.FirstOrDefault(b => b.JobId ==JobId);
+         if (ModelState.IsValid)
+        {
+            oldJob.Title = newestJob.Title;
+            oldJob.Description = newestJob.Description;
+            oldJob.Location = newestJob.Location;
+            oldJob.UpdatedAt = newestJob.UpdatedAt;
+            _context.SaveChanges();
 
+            return RedirectToAction("Dashboard",newestJob);
+        }
+        return View("EditJob", oldJob);
+
+    }
+   
+    [HttpPost("delete/{JobId}")]
+    public IActionResult DeleteJob(int JobId)
+    {
+        System.Console.WriteLine(JobId);
+            Job toDelete = _context.Jobs.FirstOrDefault(d =>d.JobId == JobId);
+            System.Console.WriteLine(toDelete==null);
+            if(toDelete == null)
+                return RedirectToAction("Dashboard");
+            _context.Jobs.Remove(toDelete);
+             _context.SaveChanges();
+            return RedirectToAction("Dashboard");
+    }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
